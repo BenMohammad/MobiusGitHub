3c978dff484679d57003a27afa14664240c45e8c
Ben Mohammad <benmohammad593@gmail.com>
2020-01-06 12:03:36 +0000

    Initial commit

---
+++ app/src/main/java/com/fukuni/mobiusgithub/domain/login/login.kt
@@ -0,0 +1,33 @@
+package com.fukuni.mobiusgithub.domain.login
+
+sealed class LoginEffect
+object GetSavedUserEffect : LoginEffect()
+data class SavedUserCredentialsEffect(val login: String, val pass: String): LoginEffect()
+data class LoginRequestEffect(val login: String, val pass: String): LoginEffect()
+object GoToMainEffect: LoginEffect()
+
+sealed class LoginEvent
+object LoginInit: LoginEvent()
+data class UserCredentialsLoadedEvent(val login: String, val pass: String): LoginEvent()
+data class UserCredentialsErrorEvent(val err: Throwable? = null): LoginEvent()
+object UserCredentialsSavedEvent: LoginEvent()
+data class LoginInputEvent(val login: String): LoginEvent()
+data class PassInputEvent(val pass: String): LoginEvent()
+data class IsSaveCredentialsEvent(val checked: Boolean): LoginEvent()
+data class LoginResponseEvent(val logged: Boolean): LoginEvent()
+data class LoginResponseErrorEvent(val err: Throwable? = null): LoginEvent()
+object LoginClickEvent: LoginEvent()
+data class NetworkStateEvent(val connected : Boolean): LoginEvent()
+
+data class LoginModel(
+
+    val login: String = "",
+    val loginError: String? = null,
+    val pass: String = "",
+    val passError: String? = null,
+    val saveUser: Boolean = false,
+    val isLoading: Boolean = true,
+    val error: String? = null,
+    val btnEnabled: Boolean = false
+
+)
---
+++ app/src/main/java/com/fukuni/mobiusgithub/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.fukuni.mobiusgithub.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fukuni.mobiusgithub.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
